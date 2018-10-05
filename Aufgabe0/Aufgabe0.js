448bfefb6d1fb9763b2aff66f269d9dc43d779c6
zythk <zythk@LAPTOP-2SPPECRB.eduroam.hs-furtwangen.de>
2018-10-05 14:12:52 +0200

    Aufgabenbearbeitung 5.10.

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", Eingabe);
+function Eingabe() {
+    var person = prompt("Geb bitte deinen Namen ein");
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! Was geht ab!";
+}
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", Eingabe);
+
+function Eingabe(): void {
+    var person: string = prompt("Geb bitte deinen Namen ein");
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! Was geht ab!";
+
+}
