6f39ebdb1c031b5192aae48f5e685f2e2930a2a9
zythk <zythk@192.168.0.46>
2018-10-10 18:36:40 +0200

    korrektur aufgabe 1

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -3,5 +3,6 @@ function Eingabe() {
     var person = prompt("Geb bitte deinen Namen ein");
     document.getElementById("demo").innerHTML =
         "Hello " + person + "! Was geht ab!";
+    console.log("Hallo" + person + "! Was geht ab!");
 }
 //# sourceMappingURL=Aufgabe0.js.map
--- Aufgabe0/Aufgabe0.ts
+++ Aufgabe0/Aufgabe0.ts
@@ -5,5 +5,6 @@ function Eingabe(): void {
     var person: string = prompt("Geb bitte deinen Namen ein");
     document.getElementById("demo").innerHTML =
         "Hello " + person + "! Was geht ab!";
-
+    console.log("Hallo" + person + "! Was geht ab!");
 }
+
