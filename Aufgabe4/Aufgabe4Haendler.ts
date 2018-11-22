210a963faa203ad141956988dc5915439ffd7b56
zythk <zythk@LAPTOP-2SPPECRB.eduroam.hs-furtwangen.de>
2018-11-22 17:47:22 +0100

    Aufgabe5

--- Aufgabe4/Aufgabe4Haendler.ts
+++ Aufgabe4/Aufgabe4Haendler.ts
@@ -9,48 +9,54 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 namespace HolyChristmasTree {
 
 
-
-
-    interface Products {
+    export interface Product {
         name: string;
         price: number;
     }
 
 
-    export let trees: Products[] = [
-        { name: "Premium Nordmanntanne", price: 34.95 },
-        { name: "Premium Blaufichte", price: 19.85 },
-        { name: "Premium Douglasie Tanne", price: 49.90 }
-    ];
+    export interface ProductGroups {
+        [key: string]: Product[];
+    }
 
-    export let treeholder: Products[] = [
-        { name: "Blumentopf", price: 20 },
-        { name: "Star-Max", price: 24.21 },
-        { name: "Antiksst‰nder", price: 16.65 }
-    ];
+    export let offers: ProductGroups =
+        {
+            "trees": [
+                { name: "Premium Nordmanntanne", price: 34.95 },
+                { name: "Premium Blaufichte", price: 19.85 },
+                { name: "Premium Douglasie Tanne", price: 49.90 }
+            ],
 
-    export let lightstring: Products[] = [
-        { name: "Sterne silber", price: 19.95 },
-        { name: "600LEDs", price: 55.55 },
-        { name: "LED Kugeln weiﬂ", price: 31.43 }
-    ];
+            "treeholder": [
+                { name: "Blumentopf", price: 20 },
+                { name: "Star-Max", price: 24.21 },
+                { name: "Antiksst‰nder", price: 16.65 }
+            ],
 
-    export let balls: Products[] = [
-        { name: "Eisblau Kunststoff", price: 1.50 },
-        { name: "Matt rot, Glas", price: 3.50 },
-        { name: "Colori Trendi", price: 5.95 }
-    ];
+            "lightstring": [
+                { name: "Sterne silber", price: 19.95 },
+                { name: "600LEDs", price: 55.55 },
+                { name: "LED Kugeln weiﬂ", price: 31.43 }
+            ],
 
-    export let lametta: Products[] = [
-        { name: "Schwarz Glizer", price: 4.99 },
-        { name: "Gold gl‰nzend", price: 4.99 },
-        { name: "Bunt  matt", price: 4.99 }
-    ];
+            "balls": [
+                { name: "Eisblau Kunststoff", price: 1.50 },
+                { name: "Matt rot, Glas", price: 3.50 },
+                { name: "Colori Trendi", price: 5.95 }
+            ],
 
-    export let top: Products[] = [
-        { name: "Aus irisierendem Klar Glas", price: 22.00 },
-        { name: "Inge Glas", price: 12.50 },
-        { name: "Satin Glas", price: 24.95 }
-    ];
+            "lametta": [
+                { name: "Schwarz Glizer", price: 4.99 },
+                { name: "Gold gl‰nzend", price: 4.99 },
+                { name: "Bunt  matt", price: 4.99 }
+            ],
 
+            "top": [
+                { name: "Aus irisierendem Klar Glas", price: 22.00 },
+                { name: "Inge Glas", price: 12.50 },
+                { name: "Satin Glas", price: 24.95 }
+            ]
+
+};
+        
 }
